358d7959f6a5a3894e00249ebf4d78783f1a3b12
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-17 20:29:48 +0100

    Display the guessed word to the end user

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const candidateWords = [
   'rhythm'
 ]
 
+const guessedWordElement = document.querySelector('#guessed_word')
 const randomWord = candidateWords[Math.floor(Math.random() * candidateWords.length)]
 let guessedWord = '-'.repeat(randomWord.length)
+guessedWordElement.textContent = guessedWord
 
 document.addEventListener('keydown', event => {
   console.log(event)
