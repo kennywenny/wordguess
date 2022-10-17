8f1276595e175aebb4f81ff9e8641c1b6618543d
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-17 20:33:42 +0100

    Tell the end user that you've pressed a button

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const candidateWords = [
 ]
 
 const guessedWordElement = document.querySelector('#guessed_word')
+const feedbackElement = document.querySelector('#feedback')
+
 const randomWord = candidateWords[Math.floor(Math.random() * candidateWords.length)]
 let guessedWord = '-'.repeat(randomWord.length)
 guessedWordElement.textContent = guessedWord
 
 document.addEventListener('keydown', event => {
+  feedbackElement.textContent = `You have pressed ${event.key}`
   console.log(event)
 })
